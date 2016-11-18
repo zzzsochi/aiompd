77b6d2b623e3a9d6fa3e78572f33d62f3fe8fb2e
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-11-18 15:16:31 +0300

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r') as f:
 
 
 setup(name='aiompd',
-      version='0.3.0',
+      version='0.3.1',
       description='MPD (Music Player Daemon) client for asyncio',
       long_description=README,
       classifiers=[
